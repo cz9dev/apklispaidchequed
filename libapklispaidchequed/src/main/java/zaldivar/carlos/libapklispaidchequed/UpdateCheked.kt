6be2695409b66570d979cef10c6258fafe1ecc31
cz9dev <czaldivarp@nauta.cu>
2022-04-27 13:12:09 -0400

    Subiendo clase para comprobar si hay nuevas versiones en apklis

---
+++ libapklispaidchequed/src/main/java/zaldivar/carlos/libapklispaidchequed/UpdateCheked.kt
@@ -0,0 +1,13 @@
+package zaldivar.carlos.libapklispaidchequed
+
+/**
+ * Clase para comprobar si hay nueva version de una aplicación en apklis
+ */
+class UpdateCheked {
+
+    companion object {
+        fun hasUpdate() {
+
+        }
+    }
+}
